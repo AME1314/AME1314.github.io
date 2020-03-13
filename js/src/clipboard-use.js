0c1b4865ac35c72c7c572d65b4546f0ab175fa2a
AME1314 <957798624@qq.com>
2020-03-13 11:13:46 +0800

    Site updated: 2020-03-13 11:13:32

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,30 @@
+/*页面载入完成后，创建复制按钮*/
+!function (e, t, a) {
+    /* code */
+    var initCopyCode = function () {
+        var copyHtml = '';
+        copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+        //fa fa-globe可以去字体库替换自己想要的图标
+        copyHtml += '  <i class="fa fa-clipboard"></i><span>复制</span>';
+        copyHtml += '</button>';
+        $(".highlight .code pre").before(copyHtml);
+        new ClipboardJS('.btn-copy', {
+            target: function (trigger) {
+                return trigger.nextElementSibling;
+            }
+        });
+        var notecopy = '';
+        notecopy += '<button class="note-copy" data-clipboard-snippet="">';
+        //fa fa-globe可以去字体库替换自己想要的图标
+        notecopy += '  <i class="fa fa-clipboard"></i><span>复制</span>';
+        notecopy += '</button>';
+        
+        $(".note").prepend(notecopy);
+        new ClipboardJS('.note-copy', {
+            target: function (trigger) {
+                return trigger.nextElementSibling;
+            }
+        });
+    }
+    initCopyCode();
+}(window, document);
